57d4b0dc025090113aca51ff2194d2b285bdebd9
Angelica Bonilla Riaño <angelicabonilla723@gmail.com>
2020-06-08 12:30:49 -0500

    feat: move project codegit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-import validator from './validator.js';
+// Seleccionar formulario y guardarlo en una variable
+var paymentForm = document.querySelector('#payment-form');
 
-console.log(validator);
+/**
+ * Agregar evento al formulario
+ * Cuando se hace click en el boton Pay
+ * El evento submit es lanzado y ejecuta
+ * nuestra funcion
+ */
+paymentForm.addEventListener('submit', onSubmit);
+
+/**
+ * Nuestra función es llamada cuando se llama
+ * el anterior evento.
+ */
+function onSubmit(event) {
+  // Se detiene el comportamiento por defecto con preventDefault
+  event.preventDefault();
+
+/**
+ * Se busca el elemento input de la pagina y se selecciona
+ * el atributo value
+ */
+  var numberInput = document.querySelector('#tc-number').value;
+
+  alert(validator(numberInput));
+}
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,35 @@
-const validator = {
-  // ...
-};
+function validator(code) {
+  // Aqui  toma la cantidad de caracteres que tenga el input
+    var len = code.length
+    //se comrpueba si la cantidad de numeros ingresados es par o impar
+    var parity = len % 2
+    // se guarda la suma de todos los digitos  hasta llegar al final 
+    var sum = 0
+    // toma el valor de la posicion indicada
+    for (var i = len-1; i >= 0; i--) {
+      //se busca el elemento en la posicion 
+        var d = parseInt(code.charAt(i))
+        /**
+         * Si es una tarjeta de 11 numeros el parity es = 1
+         * Si es una tarjeta de 12 numeros el parity es = 0
+         * Si la posicion es par y el parity es 0 entonces se
+         * multiplica por 2
+         * Si la pocision es impar y el parity es 1
+         * entonces tambien se multplica por 2
+         * 
+         * Esto quiere decir que en una tarjeta impar las 
+         * posiciones impares se multiplican por 2
+         * 
+         * En una tarjeta par las posiciones pares
+         * se multlplican por 2
+         */
+        if (i % 2 == parity) { d *= 2 }
 
-export default validator;
+        // Si el digito es mayor a 9 se le resta 9
+        if (d > 9) { d -= 9 }
+        // suma de la iteracion anterior mas el nuevo resultado
+        sum += d
+    } 
+    //si el resultado da 0, entonces la tarjeta es valida+
+    return sum % 10 === 0
+}
