c2c64bc4d06c8bba675d881f8352e10a0b5ca9d7
Angelica Bonilla Riaño <angelicabonilla723@gmail.com>
2020-06-18 11:52:00 -0500

    Updates

---
+++ index.js
@@ -0,0 +1,30 @@
+import validator from './validator.js';
+
+// Seleccionar formulario y guardarlo en una variable
+var paymentForm = document.querySelector('#payment-form');
+/**
+ * Agregar evento al formulario
+ * Cuando se hace click en el boton Pay
+ * El evento submit es lanzado y ejecuta
+ * nuestra funcion
+ */
+paymentForm.addEventListener('submit', onSubmit);
+
+/**
+ * Nuestra función es llamada cuando se llama
+ * el anterior evento.
+ */
+function onSubmit(event) {
+  // Se detiene el comportamiento por defecto con preventDefault
+  event.preventDefault();
+
+  
+/**
+ * Se busca el elemento input de la pagina y se selecciona
+ * el atributo value
+ */
+  var numberInput = document.querySelector('#tc-number').value;
+  
+  validator.maskify(numberInput);
+  alert(validator.isValid(numberInput));
+} 
